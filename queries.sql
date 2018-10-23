fd94f89c6daaca821e6d7dd6b705b8cfaa4ce0d7
Craig Kerstiens <craig.kerstiens@gmail.com>
2018-10-23 11:39:12 +0100

    updating query formatting

--- queries.sql
+++ queries.sql
@@ -1,16 +1,37 @@
---Get me the total number of events and count of distinct devices in the last 5 minutes?
+--Get me the total number of events and count of distinct devices in the last 30 minutes?
 
-SELECT sum(event_count), hll_cardinality(sum(device_distinct_count)) FROM rollup_events_5min where minute >=now()-interval '5 minutes' AND minute <=now() AND customer_id=1;
+SELECT sum(event_count), 
+       hll_cardinality(sum(device_distinct_count)) 
+FROM rollup_events_5min 
+WHERE minute >=now()-interval '30 minutes' 
+  AND minute <=now() 
+  AND customer_id=1;
 
 --Get me the count of distinct sessions over the last week?
 
-SELECT sum(event_count), hll_cardinality(sum(device_distinct_count)) FROM rollup_events_1hr where hour >=date_trunc('day',now())-interval '7 days' AND hour <=now() AND customer_id=1;
+SELECT sum(event_count), 
+       hll_cardinality(sum(device_distinct_count)) 
+FROM rollup_events_1hr 
+WHERE hour >=date_trunc('day',now())-interval '7 days' 
+  AND hour <=now() 
+  AND customer_id=1;
 
 -- Get me the trend of my app usage in the last 2 days broken by hour
 
-SELECT hour, sum(event_count) event_count, hll_cardinality(sum(device_distinct_count)) device_count, hll_cardinality(sum(session_distinct_count)) session_count FROM rollup_events_1hr where hour >=date_trunc('day',now())-interval '2 days' AND hour <=now() AND customer_id=1 GROUP BY hour;
+SELECT hour, 
+       sum(event_count) event_count, 
+       hll_cardinality(sum(device_distinct_count)) device_count, 
+       hll_cardinality(sum(session_distinct_count)) session_count 
+FROM rollup_events_1hr 
+WHERE hour >=date_trunc('day',now())-interval '2 days' 
+  AND hour <=now() 
+  AND customer_id=1 
+GROUP BY hour;
 
 -- Get me the top devices in the past 30 minutes
 SELECT (topn(topn_union_agg(top_devices_1000), 10)).item device_id
-FROM rollup_events_5min  where minute >=date_trunc('day',now())-interval '30 minutes' AND minute <=now() AND customer_id=2;
+FROM rollup_events_5min 
+WHERE minute >=date_trunc('day',now())-interval '30 minutes' 
+  AND minute <=now() 
+  AND customer_id=2;
 
